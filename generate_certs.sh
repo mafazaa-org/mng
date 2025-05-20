49c7d76886abfb3b8d97dda71f97ed3897a40f68
Ahmed <ahmedelbhairy.72005@gmail.com>
2025-05-20 22:50:30 +0300

    finished kibana setup

--- generate_certs.sh
+++ generate_certs.sh
@@ -9,3 +9,9 @@ docker run --rm -v ./c:/c docker.elastic.co/elasticsearch/elasticsearch:9.0.1 /b
     /usr/share/elasticsearch/bin/elasticsearch-certutil cert --ca /c/elastic-ca.p12 --ca-pass '$CA_PASSWORD' --out /c/elastic-certificates.p12 --pass '$CERT_PASSWORD' --name elasticsearch --dns localhost,elasticsearch --ip 127.0.0.1,172.27.0.2"
 
 openssl pkcs12 -in ./c/elastic-ca.p12 -passin "pass:$CA_PASSWORD" -out ./c/elastic-ca.pem -clcerts -nokeys
+
+openssl pkcs12 -in ./c/elastic-certificates.p12 -passin "pass:$CERT_PASSWORD" -out ./c/elastic-certificates.pem -clcerts -nokeys
+
+openssl req -x509 -newkey rsa:4096 -nodes -keyout "./c/kibana.key" -out "./c/kibana.crt" -days 365 \
+    -subj "/C=US/ST=State/L=City/O=Organization/OU=Unit/CN=localhost" \
+    -addext "subjectAltName=DNS:localhost,kibana,IP:127.0.0.1"
